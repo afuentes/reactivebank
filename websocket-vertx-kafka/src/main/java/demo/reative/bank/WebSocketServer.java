713b0d79da52e4a1c3665e27eeeaa2b5a3f3b99b
alexfuentes <fuentes.alex@gmail.com>
2020-07-29 00:21:15 -0400

    updated

---
+++ websocket-vertx-kafka/src/main/java/demo/reative/bank/WebSocketServer.java
@@ -0,0 +1,14 @@
+
+package demo.reative.bank;
+
+import io.smallrye.mutiny.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+
+public class WebSocketServer extends AbstractVerticle {
+
+  
+  @Override
+  public void start(Promise<Void> startPromise) {
+
+
+  }
