c6ae0ce82c66abf42a903203f5edd32bf2403031
alexfuentes <fuentes.alex@gmail.com>
2020-07-29 00:53:49 -0400

    reactive

--- websocket-vertx-kafka/src/main/java/demo/reative/bank/WebSocketServer.java
+++ websocket-vertx-kafka/src/main/java/demo/reative/bank/WebSocketServer.java
@@ -1,15 +1,40 @@
 
 package demo.reative.bank;
 
-import io.smallrye.mutiny.vertx.core.AbstractVerticle;
-import io.vertx.core.Promise;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.http.ServerWebSocket;
+import io.vertx.ext.web.Router;
+import io.vertx.core.json.JsonObject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class WebSocketServer extends AbstractVerticle {
-  
-  @Override
-  public void start(Promise<Void> startPromise) {
 
+  private static final Logger logger = LoggerFactory.getLogger(WebSocketServer.class);
+
+  @Override
+  public void start() {
+      final Router router = Router.router(vertx);
+
+      vertx.createHttpServer()
+      .requestHandler(router)
+      .webSocketHandler(this::handleWebSocket)
+      .listen(8080, "localhost");
+
+      logger.debug("Started server");
+      
   }
 
+  private void handleWebSocket(final ServerWebSocket webSocket) {
+    final EventBus eventBus = vertx.eventBus();
+
+    webSocket.handler(buffer -> {
+      final JsonObject message = buffer.toJsonObject();
+        eventBus.send( null, message);
+    });
+
+  }
 
 }
